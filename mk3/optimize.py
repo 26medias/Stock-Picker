fa3b98a0fdb844be8fa845741b87e715c9680611
Julien <julien@twenty-six-medias.com>
2020-10-26 22:59:29 -0400

    bugfix

--- mk3/optimize.py
+++ mk3/optimize.py
@@ -49,6 +49,11 @@ def train_evaluate(search_params):
 	sharpe = analysis.sharpe()
 	stats = sim.portfolio.summary()
 	
+	neptune.log_text('report', json.dumps(report, indent=2, sort_keys=True))
+	
+	if math.isnan(sharpe) or math.isinf(sharpe) or sharpe <= -2 or sharpe >= 5:
+		sharpe = -5
+	
 	neptune.log_metric('sharpe', sharpe)
 	neptune.log_metric('start_value', 5000)
 	neptune.log_metric('end_value', stats['total_value'])
@@ -59,11 +64,6 @@ def train_evaluate(search_params):
 		'end_value': stats['total_value'],
 		'gains': (stats['total_value']-5000.0)/5000.0
 	}
-	
-	neptune.log_text('report', json.dumps(report, indent=2, sort_keys=True))
-	
-	if math.isnan(sharpe) or math.isinf(sharpe):
-		return 0
 	
 	return sharpe
 
